275189516a38aa676ba1f97c1fccd61412bbfade
Harry Cruz <harry@rhesis.ai>
2025-05-24 15:20:17 +0000

    Removing detailed prompt metric because we focus on one metric at a time.

--- apps/backend/src/rhesis/backend/metrics/rhesis/__init__.py
+++ apps/backend/src/rhesis/backend/metrics/rhesis/__init__.py
@@ -4,16 +4,13 @@ from .factory import RhesisMetricFactory
 from .metric_base import RhesisMetricBase
 from .prompt_metric import (
     RhesisPromptMetric, 
-    RhesisDetailedPromptMetric, 
     ScoreResponse, 
-    DetailedScoreResponse
 )
 
 __all__ = [
     "RhesisMetricBase",
     "RhesisMetricFactory",
     "RhesisPromptMetric",
-    "RhesisDetailedPromptMetric",
     "ScoreResponse",
     "DetailedScoreResponse",
 ] 
