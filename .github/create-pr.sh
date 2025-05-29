318625268f092b7ca4bc478f0cadfcb2d8984860
Harry Cruz <harry@rhesis.ai>
2025-05-29 10:36:13 +0000

    Removing tests from capitalization

--- .github/create-pr.sh
+++ .github/create-pr.sh
@@ -77,7 +77,7 @@ generate_title() {
         "API" "REST" "HTTP" "HTTPS" "URL" "URI" "GRPC"
         "UI" "UX" "CSS" "HTML" "JS" "TS" "JSX" "TSX"
         "DB" "SQL" "AWS" "GCP" "AZURE" "K8S" "DOCKER"
-        "CI" "CD" "QA" "QC" "TEST" "TESTS"
+        "CI" "CD" "QA" "QC"
         "JWT" "AUTH" "OAUTH" "SSO" "LDAP"
         "JSON" "XML" "YAML" "YML" "CSV" "PDF"
         "SDK" "CLI" "GUI" "IDE" "VM" "VPC" "DNS"
