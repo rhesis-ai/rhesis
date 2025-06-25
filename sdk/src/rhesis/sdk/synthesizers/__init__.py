9af7cb78d4615eddec5515bd39fd949000083b3b
Harry Cruz <harry@rhesis.ai>
2025-06-25 16:55:10 +0000

    Adding synthesizer factory to SDK

--- sdk/src/rhesis/sdk/synthesizers/__init__.py
+++ sdk/src/rhesis/sdk/synthesizers/__init__.py
@@ -1,5 +1,6 @@
 from rhesis.sdk.synthesizers.base import TestSetSynthesizer
 from rhesis.sdk.synthesizers.paraphrasing_synthesizer import ParaphrasingSynthesizer
 from rhesis.sdk.synthesizers.prompt_synthesizer import PromptSynthesizer
+from rhesis.sdk.synthesizers.factory import SynthesizerFactory, SynthesizerType
 
-__all__ = ["TestSetSynthesizer", "PromptSynthesizer", "ParaphrasingSynthesizer"]
+__all__ = ["TestSetSynthesizer", "PromptSynthesizer", "ParaphrasingSynthesizer", "SynthesizerFactory", "SynthesizerType"]
