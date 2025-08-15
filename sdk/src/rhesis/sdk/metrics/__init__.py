50ebfd0e06630c6e76342623d13b95c6b94ff929
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-08-15 10:33:05 +0200

    copy metrics from the backend to the sdk

---
+++ sdk/src/rhesis/sdk/metrics/__init__.py
@@ -0,0 +1,71 @@
+"""Metrics for evaluating RAG and generation systems."""
+
+from .base import BaseMetric, MetricConfig, MetricResult
+from .config.loader import MetricConfigLoader
+from .evaluator import MetricEvaluator as Evaluator
+from .factory import MetricFactory
+from .score_evaluator import ScoreEvaluator
+from .constants import ScoreType, ThresholdOperator, OPERATOR_MAP, VALID_OPERATORS_BY_SCORE_TYPE
+from .utils import run_evaluation, diagnose_invalid_metric
+from .rhesis import (  # Re-export Rhesis metrics
+    RhesisMetricBase,
+    RhesisMetricFactory,
+    RhesisPromptMetric,
+)
+# Lazy import to avoid circular dependencies
+# from .deepeval import (  # Re-export DeepEval metrics
+#     DeepEvalMetricBase,
+#     DeepEvalMetricFactory,
+#     DeepEvalAnswerRelevancy,
+#     DeepEvalFaithfulness,
+#     DeepEvalContextualRelevancy,
+#     DeepEvalContextualPrecision,
+#     DeepEvalContextualRecall,
+# )
+from .ragas import (  # Re-export Ragas metrics
+    RagasMetricBase,
+    RagasMetricFactory,
+    RagasAnswerRelevancy,
+    RagasContextualPrecision,
+)
+
+__all__ = [
+    # Base metrics
+    "BaseMetric",
+    "MetricConfig", 
+    "MetricResult",
+    "MetricConfigLoader",
+    "MetricFactory",
+    
+    # Evaluation
+    "Evaluator",
+    "ScoreEvaluator",
+    "run_evaluation",
+    
+    # Types and utilities
+    "ScoreType",
+    "ThresholdOperator",
+    "OPERATOR_MAP",
+    "VALID_OPERATORS_BY_SCORE_TYPE",
+    "diagnose_invalid_metric",
+    
+    # Rhesis metrics
+    "RhesisMetricBase",
+    "RhesisMetricFactory",
+    "RhesisPromptMetric",
+    
+    # DeepEval metrics (commented out to avoid circular imports)
+    # "DeepEvalMetricBase",
+    # "DeepEvalMetricFactory", 
+    # "DeepEvalAnswerRelevancy",
+    # "DeepEvalFaithfulness",
+    # "DeepEvalContextualRelevancy",
+    # "DeepEvalContextualPrecision",
+    # "DeepEvalContextualRecall",
+    
+    # Ragas metrics
+    "RagasMetricBase",
+    "RagasMetricFactory",
+    "RagasAnswerRelevancy",
+    "RagasContextualPrecision",
+]
