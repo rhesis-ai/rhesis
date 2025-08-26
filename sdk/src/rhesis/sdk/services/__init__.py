34372588c3e0b7326dd0c7a348b535493e0ec8b9
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-08-26 16:01:15 +0200

    introduce llm service

--- sdk/src/rhesis/sdk/services/__init__.py
+++ sdk/src/rhesis/sdk/services/__init__.py
@@ -1,4 +1,6 @@
 from .extractor import DocumentExtractor
 from .llm import LLMService
+from .providers.rhesis_premium import RhesisPremiumLLMService
+from .providers.rhesis_provider import RhesisLLMService
 
-__all__ = ["DocumentExtractor", "LLMService"]
+__all__ = ["DocumentExtractor", "LLMService", "RhesisLLMService", "RhesisPremiumLLMService"]
