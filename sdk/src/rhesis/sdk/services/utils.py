0aa8fdf35f67d6720b2f8cce1c630a941ca0e59c
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-01 14:41:15 +0200

    add sdk/service/utils tests

--- sdk/src/rhesis/sdk/services/utils.py
+++ sdk/src/rhesis/sdk/services/utils.py
@@ -5,8 +5,7 @@ from pydantic import BaseModel
 
 
 def validate_llm_response(data: dict, schema: Union[BaseModel, dict]) -> dict:
-    if isinstance(schema, BaseModel):
-        data = schema.model_validate(data)
+    if issubclass(schema, BaseModel):
+        schema.model_validate(data)
     elif isinstance(schema, dict):
-        data = jsonschema.validate(data, schema["json_schema"])
-    return data
+        jsonschema.validate(data, schema["json_schema"])
