dfd0c898a6da7390f203cf44617377e9f292791a
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-02 14:07:37 +0200

    remove LLM Service, as it was developed in another branch

--- sdk/src/rhesis/sdk/services/__init__.py
+++ sdk/src/rhesis/sdk/services/__init__.py
@@ -1,6 +1,4 @@
 from .extractor import DocumentExtractor
 from .llm import LLMService
-from .providers.rhesis_premium import RhesisPremiumLLMService
-from .providers.rhesis_provider import RhesisLLMService
 
-__all__ = ["DocumentExtractor", "LLMService", "RhesisLLMService", "RhesisPremiumLLMService"]
+__all__ = ["DocumentExtractor", "LLMService"]
