d0422b0d45e15566fe25aabc994ec70b2fc8cbf6
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-03 15:47:07 +0200

    rename openai_provider

--- sdk/src/rhesis/sdk/models/__init__.py
+++ sdk/src/rhesis/sdk/models/__init__.py
@@ -1,5 +1,5 @@
 from rhesis.sdk.models.providers.gemini import GeminiLLM
-from rhesis.sdk.models.providers.openai_provider import OpenAILLM
+from rhesis.sdk.models.providers.openai import OpenAILLM
 from rhesis.sdk.models.providers.rhesis_provider import RhesisLLM
 
 __all__ = ["RhesisLLM", "GeminiLLM", "OpenAILLM"]
