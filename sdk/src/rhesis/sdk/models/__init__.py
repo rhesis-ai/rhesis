d6caad3e26a4bf946f7d061ff4f983d1b848b8d6
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-03 15:53:52 +0200

    sort imports

--- sdk/src/rhesis/sdk/models/__init__.py
+++ sdk/src/rhesis/sdk/models/__init__.py
@@ -1,5 +1,5 @@
 from rhesis.sdk.models.providers.gemini import GeminiLLM
-from rhesis.sdk.models.providers.openai import OpenAILLM
 from rhesis.sdk.models.providers.native import RhesisLLM
+from rhesis.sdk.models.providers.openai import OpenAILLM
 
 __all__ = ["RhesisLLM", "GeminiLLM", "OpenAILLM"]
