ae47493efc4dcaa48eabf6c2b15662a85d17e788
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-25 23:03:41 +0200

    feat: add entity config

--- sdk/src/rhesis/sdk/entities/behavior.py
+++ sdk/src/rhesis/sdk/entities/behavior.py
@@ -1,10 +1,19 @@
 from typing import Any
 
+from pydantic import BaseModel, Field
+
 from rhesis.sdk.client import Endpoints
 from rhesis.sdk.entities.base_collection import BaseCollection
 from rhesis.sdk.entities.base_entity import BaseEntity
+from rhesis.sdk.utils import generate_nano_id
 
 ENDPOINT = Endpoints.BEHAVIORS
+
+
+class BehaviorConfig(BaseModel):
+    name: str
+    description: str
+    nano_id: str = Field(default_factory=generate_nano_id)
 
 
 class Behavior(BaseEntity):
@@ -16,3 +25,8 @@ class Behavior(BaseEntity):
 
 class Behaviors(BaseCollection):
     endpoint = ENDPOINT
+
+
+if __name__ == "__main__":
+    a = BehaviorConfig(name="Test Behavior", description="Test Description")
+    print(a.nano_id)
