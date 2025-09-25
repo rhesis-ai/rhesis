573b6c554680ef8214a14dc82e872d908f3ac790
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-25 23:03:41 +0200

    feat: add entity config

--- sdk/src/rhesis/sdk/entities/topic.py
+++ sdk/src/rhesis/sdk/entities/topic.py
@@ -1,10 +1,19 @@
 from typing import Any
 
+from pydantic import BaseModel, Field
+
 from rhesis.sdk.client import Endpoints
 from rhesis.sdk.entities.base_collection import BaseCollection
 from rhesis.sdk.entities.base_entity import BaseEntity
+from rhesis.sdk.utils import generate_nano_id
 
 ENDPOINT = Endpoints.TOPICS
+
+
+class TopicConfig(BaseModel):
+    name: str
+    description: str
+    nano_id: str = Field(default_factory=generate_nano_id)
 
 
 class Topic(BaseEntity):
