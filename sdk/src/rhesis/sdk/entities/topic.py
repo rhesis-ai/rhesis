a586ad79006e55a364c9ff1ce0e315666fdc557e
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-25 22:35:59 +0200

    refactor: introduce constant ENDPOINT

--- sdk/src/rhesis/sdk/entities/topic.py
+++ sdk/src/rhesis/sdk/entities/topic.py
@@ -4,13 +4,15 @@ from rhesis.sdk.client import Endpoints
 from rhesis.sdk.entities.base_collection import BaseCollection
 from rhesis.sdk.entities.base_entity import BaseEntity
 
+ENDPOINT = Endpoints.TOPICS
+
 
 class Topic(BaseEntity):
-    endpoint = Endpoints.TOPICS
+    endpoint = ENDPOINT
 
     def __init__(self, **fields: Any) -> None:
         super().__init__(**fields)
 
 
 class Topics(BaseCollection):
-    endpoint = Endpoints.TOPICS
+    endpoint = ENDPOINT
