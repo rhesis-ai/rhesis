df2266976e321f067bcd3ef31d26acd21e0e4273
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-25 21:22:40 +0200

    feat: test base_entity

--- tests/sdk/entities/test_base_entity.py
+++ tests/sdk/entities/test_base_entity.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 from unittest.mock import patch
 
 from rhesis.sdk.entities.base_entity import BaseEntity
@@ -7,20 +8,83 @@ os.environ["RHESIS_API_KEY"] = "test_api_key"
 os.environ["RHESIS_BASE_URL"] = "http://test:8000"
 
 
+class TestEndpoint(Enum):
+    TEST = "test"
+
+
 class TestEntity(BaseEntity):
-    endpoint = "test"
+    endpoint = TestEndpoint.TEST
 
 
-@patch("requests.api.request")
+@patch("requests.request")
 def test_delete(mock_request):
     record_id = 1
     entity = TestEntity()
     entity.delete(record_id)
     mock_request.assert_called_once_with(
-        "delete",
-        "http://test:8000/test/1/",
+        method="DELETE",
+        url="http://test:8000/test/1",
         headers={
             "Authorization": "Bearer test_api_key",
             "Content-Type": "application/json",
         },
+        json=None,
+        params=None,
     )
+
+
+@patch("requests.request")
+def test_save_with_id(mock_request):
+    entity = TestEntity()
+    entity.fields = {"id": 1, "name": "Test", "description": "Test"}
+    entity.save()
+    mock_request.assert_called_once_with(
+        method="PUT",
+        url="http://test:8000/test/1",
+        headers={
+            "Authorization": "Bearer test_api_key",
+            "Content-Type": "application/json",
+        },
+        json={"name": "Test", "description": "Test"},
+        params=None,
+    )
+
+
+@patch("requests.request")
+def test_save_without_id(mock_request):
+    entity = TestEntity()
+    entity.fields = {"name": "Test", "description": "Test"}
+    entity.save()
+    mock_request.assert_called_once_with(
+        method="POST",
+        url="http://test:8000/test",
+        headers={
+            "Authorization": "Bearer test_api_key",
+            "Content-Type": "application/json",
+        },
+        json={"name": "Test", "description": "Test"},
+        params=None,
+    )
+
+
+@patch("requests.request")
+def test_fetch(mock_request):
+    mock_request.return_value.json.return_value = {
+        "id": 2,
+        "name": "Test2",
+        "description": "Test2",
+    }
+    entity = TestEntity()
+    entity.fields = {"id": 1, "name": "Test", "description": "Test"}
+    entity.fetch()
+    mock_request.assert_called_once_with(
+        method="GET",
+        url="http://test:8000/test/1",
+        headers={
+            "Authorization": "Bearer test_api_key",
+            "Content-Type": "application/json",
+        },
+        json=None,
+        params=None,
+    )
+    assert entity.fields == {"id": 2, "name": "Test2", "description": "Test2"}
