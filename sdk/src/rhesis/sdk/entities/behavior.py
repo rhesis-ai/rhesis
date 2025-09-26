41bc1825070659878c982ce42d39a55d59f44449
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-09-26 18:36:10 +0200

    refactor: rename

--- sdk/src/rhesis/sdk/entities/behavior.py
+++ sdk/src/rhesis/sdk/entities/behavior.py
@@ -1,26 +1,29 @@
-from typing import Any
+from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from rhesis.sdk.client import Endpoints
 from rhesis.sdk.entities.base_collection import BaseCollection
 from rhesis.sdk.entities.base_entity import BaseEntity
-from rhesis.sdk.utils import generate_nano_id
 
 ENDPOINT = Endpoints.BEHAVIORS
 
 
-class BehaviorConfig(BaseModel):
+class BehaviorSchema(BaseModel):
     name: str
     description: str
-    nano_id: str = Field(default_factory=generate_nano_id)
+    id: Optional[str] = None
 
 
 class Behavior(BaseEntity):
     endpoint = ENDPOINT
+    entity_schema = BehaviorSchema
 
-    def __init__(self, **fields: Any) -> None:
-        super().__init__(**fields)
+    def __init__(self, name: str, description: str, id: Optional[str] = None) -> None:
+        self.name = name
+        self.description = description
+        self.id = id
+        self._set_fields()
 
 
 class Behaviors(BaseCollection):
