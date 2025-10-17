142eaf22afcf337f5c3989fccd08eeb75c4d2ece
Alexey <alexey@rhesis.ai>
2025-10-17 13:14:19 +0200

    refactor: formatting and warnings

--- apps/polyphemus/src/rhesis/polyphemus/benchmarking/models/__init__.py
+++ apps/polyphemus/src/rhesis/polyphemus/benchmarking/models/__init__.py
@@ -2,4 +2,6 @@
 from .dolphin3 import Dolphin3
 from .hermes3 import Hermes3
 from .deep_hermes3 import DeepHermes3
+from .judge import Judge
 
+__all__ = ["Dolphin3", "Hermes3", "DeepHermes3", "Judge"]
