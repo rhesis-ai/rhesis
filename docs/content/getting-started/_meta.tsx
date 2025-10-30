0ad0a92bc423e1e31850f814d27189327085b19c
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-10-30 10:27:32 +0100

    docs: remove outdated documents

--- docs/content/getting-started/_meta.tsx
+++ docs/content/getting-started/_meta.tsx
@@ -1,8 +1,8 @@
 import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
-  index: 'Overview',
   concepts: 'Core Concepts',
+  'self-hosting': 'Self-hosting',
 }
 
 export default meta
