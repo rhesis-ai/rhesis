f501828f118c4e30e3f8f33f54004c6771c65405
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-10-31 18:54:32 +0100

    ci: clean tests

--- tests/sdk/test_integration.py
+++
@@ -1,6 +0,0 @@
-from rhesis.sdk.client import Endpoints, Methods
-
-
-def test_integration_client_works(integration_client):
-    response = integration_client.send_request(endpoint=Endpoints.HEALTH, method=Methods.GET)
-    assert response["status"] == "ok"
