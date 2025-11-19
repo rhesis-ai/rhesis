76aea9f62368eb8d80dde46ea365503a27584e37
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@rhesis.ai>
2025-11-19 17:04:12 +0100

    fix: add backend dev docker file, change tests ports

--- tests/sdk/conftest.py
+++ tests/sdk/conftest.py
@@ -3,8 +3,9 @@
 import pytest
 
 
+# RHESIS_API_KEY should not be changed as it is used in integration tests auth
 @pytest.fixture(autouse=True)
 def set_api_keys(monkeypatch):
     """Automatically set API keys for all SDK tests."""
-    monkeypatch.setenv("RHESIS_API_KEY", "test_api_key")
+    monkeypatch.setenv("RHESIS_API_KEY", "rh-test-token")
     monkeypatch.setenv("GEMINI_API_KEY", "test_api_key")
