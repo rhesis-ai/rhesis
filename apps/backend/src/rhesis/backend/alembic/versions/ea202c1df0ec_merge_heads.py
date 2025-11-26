275fa7624927b352e07875ec849544782d39018a
Emanuele De Rossi <emanuele.derossi@rhesis.ai>
2025-11-26 09:06:23 +0100

    style: fix formatting

--- apps/backend/src/rhesis/backend/alembic/versions/ea202c1df0ec_merge_heads.py
+++ apps/backend/src/rhesis/backend/alembic/versions/ea202c1df0ec_merge_heads.py
@@ -4,10 +4,9 @@ from typing import Union, Sequence
 import rhesis
 
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'ea202c1df0ec'
-down_revision: Union[str, None] = ('dc03001dbf24', 'f25f0800e881')
+revision: str = "ea202c1df0ec"
+down_revision: Union[str, None] = ("dc03001dbf24", "f25f0800e881")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
