8733e130ea56cc890d3fcd5625fe6721b96ff166
Emanuele De Rossi <emanuele.derossi@rhesis.ai>
2025-12-16 12:50:21 +0100

    docs: add new dashboard page
    - add a new dashboard page with short video

--- docs/content/platform/_meta.tsx
+++ docs/content/platform/_meta.tsx
@@ -2,6 +2,7 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   index: "Overview",
+  dashboard: "Dashboard",
   organizations: "Organizations & Team",
   "---": {
     type: "separator",
