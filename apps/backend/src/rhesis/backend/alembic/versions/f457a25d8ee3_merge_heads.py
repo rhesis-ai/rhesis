3d956c6337ade5000651163ee2d0ce489c6edfa6
Emanuele De Rossi <emanuele.derossi@rhesis.ai>
2026-04-01 09:12:04 +0200

    fix: formatting

--- apps/backend/src/rhesis/backend/alembic/versions/f457a25d8ee3_merge_heads.py
+++ apps/backend/src/rhesis/backend/alembic/versions/f457a25d8ee3_merge_heads.py
@@ -1,13 +1,8 @@
-from alembic import op
-import sqlalchemy as sa
-from typing import Union, Sequence
-import rhesis
-
-
+from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = 'f457a25d8ee3'
-down_revision: Union[str, None] = ('533ebb47f308', '8993eecbb913')
+revision: str = "f457a25d8ee3"
+down_revision: Union[str, None] = ("533ebb47f308", "8993eecbb913")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
