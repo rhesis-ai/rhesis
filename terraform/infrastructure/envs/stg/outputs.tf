cc724a79bf5b6d48facc6a6ba770824b161c0c87
Md Asaduzzaman Miah <asad.miah@rhesis.ai>
2026-04-27 10:30:45 +0200

    remove redundent code for the bucket addition

--- terraform/infrastructure/envs/stg/outputs.tf
+++ terraform/infrastructure/envs/stg/outputs.tf
@@ -33,23 +33,3 @@ output "external_dns_secret_id" {
 output "internal_dns_tsig_key_secret_id" {
   value = module.internal_dns_stg.tsig_key_secret_id
 }
-
-output "file_storage_bucket_name" {
-  value       = module.gcs_stg.file_storage_bucket_name
-  description = "GCS bucket for backend file storage; match STORAGE_SERVICE_URI in secrets"
-}
-
-output "file_storage_uri" {
-  value       = module.gcs_stg.file_storage_uri
-  description = "gs:// URI for file storage (STORAGE_SERVICE_URI)"
-}
-
-output "cnpg_backup_bucket_name" {
-  value       = module.gcs_stg.cnpg_backup_bucket_name
-  description = "GCS bucket for CNPG (Barman) database backups"
-}
-
-output "cnpg_backup_uri" {
-  value       = module.gcs_stg.cnpg_backup_uri
-  description = "gs:// URI for CNPG backup destination; use in Cluster backup configuration"
-}
