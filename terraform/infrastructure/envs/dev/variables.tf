3beb441971eef09ee2228feb69d48581ae208cac
Md Asaduzzaman Miah <asad.miah@rhesis.ai>
2026-06-01 14:26:25 +0200

    add: bucket creation for dev by terraform

--- terraform/infrastructure/envs/dev/variables.tf
+++ terraform/infrastructure/envs/dev/variables.tf
@@ -14,3 +14,15 @@ variable "gke_deletion_protection" {
   default     = false
 }
 
+variable "file_storage_bucket_name" {
+  description = "GCS bucket name for Rhesis backend file storage"
+  type        = string
+  default     = "sources-files-rhesis-dev"
+}
+
+variable "force_destroy" {
+  description = "Allow bucket destruction even when non-empty (set false for production data)"
+  type        = bool
+  default     = false
+}
+
