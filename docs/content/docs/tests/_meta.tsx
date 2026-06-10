b265ae716197c8816b828dea052d5f6ab803c989
EmanueleDeRossi1 <emanuelederossi313@gmail.com>
2026-06-10 15:36:24 +0200

    docs(tests): nest test execution and test variant pages under Tests

---
+++ docs/content/docs/tests/_meta.tsx
@@ -0,0 +1,11 @@
+import type { MetaRecord } from 'nextra'
+
+const meta: MetaRecord = {
+  index: 'Tests',
+  execution: 'Test Execution',
+  'conversation-simulation': 'Conversation Simulation',
+  'adversarial-testing': 'Adversarial Testing',
+  'multimodal-testing': 'Multi-modal Testing',
+}
+
+export default meta
