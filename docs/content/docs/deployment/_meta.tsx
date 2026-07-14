97128e73f76271d2a74fc1c4902ab2ea51aca755
Md Asaduzzaman Miah <asad.miah@rhesis.ai>
2026-07-14 11:24:49 +0200

    fix: kubernetes deployment guide

--- docs/content/docs/deployment/_meta.tsx
+++ docs/content/docs/deployment/_meta.tsx
@@ -2,5 +2,8 @@ export default {
   index: 'Overview',
   'quick-start': 'Quick Start',
   'docker-compose': 'Docker Compose',
+  'gcp-terraform': 'GCP (Terraform)',
+  'kubernetes-helm': 'Kubernetes (Helm)',
   'environment-variables': 'Environment Variables'
+  
 }
