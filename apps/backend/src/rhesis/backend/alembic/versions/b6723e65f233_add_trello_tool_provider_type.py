6ac250e058ed3e1f5d3a8e78e0a93471c0b47c71
sujalgawas <sujalgawas18@gmail.com>
2026-09-11 03:53:38 +0000

    fix: updated migration down_revision
    Signed-off-by: sujalgawas <sujalgawas18@gmail.com>

--- apps/backend/src/rhesis/backend/alembic/versions/b6723e65f233_add_trello_tool_provider_type.py
+++ apps/backend/src/rhesis/backend/alembic/versions/b6723e65f233_add_trello_tool_provider_type.py
@@ -1,6 +1,6 @@
 """Add Trello ToolProviderType
 Revision ID: b6723e65f233
-Revises: b258958270d0
+Revises: b7e1c9d4a2f3
 Create Date: 2026-09-02
 """
 
@@ -15,7 +15,7 @@ from rhesis.backend.alembic.utils.template_loader import (
 
 # revision identifiers, used by Alembic.
 revision: str = "b6723e65f233"
-down_revision: Union[str, None] = "b258958270d0"
+down_revision: Union[str, None] = "b7e1c9d4a2f3"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
